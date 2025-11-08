2f9919b519f4332474926433545186e86044974e
riccione <riccione@protonmail.com>
2025-11-07 22:42:05 -0500

    refactor(ingestion): add more supported extensions for text_extractor

--- mnemolet_core/ingestion/extractors/text_extractor.py
+++ mnemolet_core/ingestion/extractors/text_extractor.py
@@ -5,7 +5,47 @@ from .base import Extractor
 
 
 class TextExtractor(Extractor):
-    extensions = {".txt"}
+    extensions = {
+        # plain text
+        ".txt",
+        ".log",
+        ".conf",
+        ".ini",
+        ".json",
+        ".yml",
+        ".toml",
+        # code
+        ".py",
+        ".js",
+        ".ts",
+        ".java",
+        ".cpp",
+        ".c",
+        ".h",
+        ".hpp",
+        ".rs",
+        ".go",
+        ".sh",
+        ".bash",
+        ".ps1",
+        ".rb",
+        ".php",
+        "pl",
+        # markdown
+        # TODO: need additional work for csv and html
+        ".md",
+        ".html",
+        ".xml",
+        ".csv",
+        ".tsv",
+        ".css",
+        # documentation
+        ".rst",
+        ".tex",
+        ".jinja",
+        ".jinja2",
+        ".tpl",
+    }
 
     def extract(self, file: Path) -> Iterator[str]:
         """
